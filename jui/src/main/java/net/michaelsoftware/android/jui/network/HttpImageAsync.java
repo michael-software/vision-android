b5abc27d01a42d7d835b5e8070b7d065e4260f0d
Michael Braun <michaelsoftware1997@gmail.com>
2016-11-03 08:01:06 +0100

    Added cache for HttpImageAsync

--- jui/src/main/java/net/michaelsoftware/android/jui/network/HttpImageAsync.java
+++ jui/src/main/java/net/michaelsoftware/android/jui/network/HttpImageAsync.java
@@ -4,13 +4,31 @@ import android.app.ProgressDialog;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
+import android.os.Environment;
+import android.util.Log;
 import android.widget.ImageView;
 
 import net.michaelsoftware.android.jui.Tools;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.net.ssl.HttpsURLConnection;
 
 /**
  * Created by Michael on 31.08.2016.
@@ -20,9 +38,20 @@ public class HttpImageAsync extends AsyncTask<String,Void,Bitmap> {
     ProgressDialog loading;
     private int width;
     private int height;
+    private Object object;
+    private String method;
+
+    File SDCardRoot = Environment.getExternalStorageDirectory().getAbsoluteFile();
+    private ArrayList<String> specialData;
+
 
     public HttpImageAsync(ImageView imageView) {
         this.imageView = imageView;
+    }
+
+    public HttpImageAsync(Object object, String method) {
+        this.object = object;
+        this.method = method;
     }
 
     public void setWidth(int width) {
@@ -38,22 +67,72 @@ public class HttpImageAsync extends AsyncTask<String,Void,Bitmap> {
         URL url = null;
         Bitmap image = null;
 
+
+        Log.d("test", params[0]);
+
+
         String urlToImage = params[0];
         try {
             url = new URL(urlToImage);
 
-            final BitmapFactory.Options options = new BitmapFactory.Options();
+            File folder = this.getFolder(url);
+            File file = this.getFile(url);
 
-            if(this.height > 0 && this.width > 0) {
-                options.inJustDecodeBounds = true;
-                BitmapFactory.decodeStream(url.openConnection().getInputStream(), null, options);
 
-                options.inSampleSize = Tools.calculateInSampleSize(options, this.width, this.height);
+            if(file != null && file.exists() && file.isFile()) {
+
+
+                FileInputStream fileInputStream = new FileInputStream(file);
+
+                final BitmapFactory.Options options = new BitmapFactory.Options();
+
+                if (this.height > 0 && this.width > 0) {
+                    options.inJustDecodeBounds = true;
+                    BitmapFactory.decodeStream(fileInputStream, null, options);
+
+                    options.inSampleSize = Tools.calculateInSampleSize(options, this.width, this.height);
+                }
+
+                // Decode bitmap with inSampleSize set
+                options.inJustDecodeBounds = false;
+                image = BitmapFactory.decodeStream(new FileInputStream(file), null, options);
+
+
+            } else {
+
+                URLConnection urlConnection = url.openConnection();
+                InputStream inStream = url.openConnection().getInputStream();
+
+                final BitmapFactory.Options options = new BitmapFactory.Options();
+
+                if (this.height > 0 && this.width > 0) {
+                    options.inJustDecodeBounds = true;
+                    BitmapFactory.decodeStream(inStream, null, options);
+
+                    options.inSampleSize = Tools.calculateInSampleSize(options, this.width, this.height);
+                }
+
+                // Decode bitmap with inSampleSize set
+                options.inJustDecodeBounds = false;
+                image = BitmapFactory.decodeStream(urlConnection.getInputStream(), null, options);
+
+
+                if (file != null) {
+                    FileOutputStream fileOutput = new FileOutputStream(file);
+                    InputStream inputStream = urlConnection.getInputStream();
+                    int totalSize = urlConnection.getContentLength();
+                    int downloadedSize = 0;
+                    byte[] buffer = new byte[1024];
+                    int bufferLength = 0;
+                    while ((bufferLength = inputStream.read(buffer)) > 0) {
+                        fileOutput.write(buffer, 0, bufferLength);
+                        downloadedSize += bufferLength;
+                        Log.i("Progress:", "downloadedSize:" + downloadedSize + "totalSize:" + totalSize);
+                    }
+                    fileOutput.close();
+                }
+
             }
-
-            // Decode bitmap with inSampleSize set
-            options.inJustDecodeBounds = false;
-            image =  BitmapFactory.decodeStream(url.openConnection().getInputStream(), null, options);
 
             //image = BitmapFactory.decodeStream(url.openConnection().getInputStream());
         } catch (MalformedURLException e) {
@@ -62,6 +141,34 @@ public class HttpImageAsync extends AsyncTask<String,Void,Bitmap> {
             e.printStackTrace();
         }
         return image;
+    }
+
+    private File getFolder(URL url) {
+        String folder = url.getHost() + "/cache/" + Tools.getPath( url.getPath() );
+        File fileFolder = new File(SDCardRoot, folder);
+        fileFolder.mkdirs();
+
+        return fileFolder;
+    }
+
+
+    private File getFile(URL url) {
+        String file = url.getHost() + "/cache/" + Tools.getPath( url.getPath() ) + Tools.getFilename( url.getFile(), url.getQuery());
+        //String filename="downloadedFile.png";
+        Log.i("Local filename:",""+file);
+
+
+        File fileFile = new File(SDCardRoot, file);
+        try {
+            if(fileFile.exists() || fileFile.createNewFile())
+            {
+                return fileFile;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 
     @Override
@@ -74,6 +181,33 @@ public class HttpImageAsync extends AsyncTask<String,Void,Bitmap> {
     protected void onPostExecute(Bitmap bitmap) {
         super.onPostExecute(bitmap);
         //loading.dismiss();
-        imageView.setImageBitmap(bitmap);
+
+        if(object != null && method != null) {
+            try {
+                if(this.specialData == null || this.specialData.size() == 0) {
+                    Method method = this.object.getClass().getMethod(this.method, Bitmap.class);
+                    method.invoke(this.object, bitmap);
+                } else {
+                    Method method = this.object.getClass().getMethod(this.method, Bitmap.class, ArrayList.class);
+                    method.invoke(this.object, bitmap, this.specialData);
+                }
+
+                return;
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if(imageView != null) {
+            imageView.setImageBitmap(bitmap);
+        }
+    }
+
+    public void setSpecialData(ArrayList<String> specialData) {
+        this.specialData = specialData;
     }
 }
