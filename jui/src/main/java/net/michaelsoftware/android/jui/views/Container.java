96c83a60b18cddcd2bab023aafedac8f14390af4
Michael Braun <michaelsoftware1997@gmail.com>
2016-11-24 22:13:40 +0100

    Added click and longclick to container

--- jui/src/main/java/net/michaelsoftware/android/jui/views/Container.java
+++ jui/src/main/java/net/michaelsoftware/android/jui/views/Container.java
@@ -7,6 +7,8 @@ import android.widget.LinearLayout;
 
 import net.michaelsoftware.android.jui.JuiParser;
 import net.michaelsoftware.android.jui.Tools;
+import net.michaelsoftware.android.jui.listeners.CustomOnClickListener;
+import net.michaelsoftware.android.jui.listeners.CustomOnLongClickListener;
 
 import java.util.HashMap;
 
@@ -16,6 +18,8 @@ import java.util.HashMap;
 public class Container extends JuiView {
     private HashMap<Object, Object> value = null;
     private HashMap<Object, Object> properties = null;
+    private String click;
+    private String longclick;
 
     public Container(Context activity) {
         super(activity);
@@ -27,6 +31,14 @@ public class Container extends JuiView {
         if(Tools.isHashmap(hashMap.get("value"))) {
             this.setValue((HashMap<Object, Object>) hashMap.get("value"));
 
+            if (Tools.isString(hashMap.get("click"))) {
+                this.setClick((String) hashMap.get("click"));
+            }
+
+            if (Tools.isString(hashMap.get("longclick"))) {
+                this.setLongClick((String) hashMap.get("longclick"));
+            }
+
             properties = hashMap;
         }
     }
@@ -35,10 +47,27 @@ public class Container extends JuiView {
     public View getView(JuiParser parser) {
         if(value != null) {
             LinearLayout lin =  parser.parseReturn(value);
+
+            if (!Tools.empty(click)) {
+                lin.setOnClickListener(new CustomOnClickListener(parser, click));
+            }
+
+            if (!Tools.empty(longclick)) {
+                lin.setOnLongClickListener(new CustomOnLongClickListener(parser, longclick));
+            }
+
             return JuiParser.addProperties(lin, properties);
         }
 
         return null;
+    }
+
+    public void setClick(String click) {
+        this.click = click;
+    }
+
+    public void setLongClick(String longClick) {
+        this.longclick = longClick;
     }
 
     public void setValue(HashMap<Object, Object> value) {
